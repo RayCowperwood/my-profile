38a5c0ea74786efeab3f7f829811b452dff8b78d
Andrii Tseplyi <and.tseplyi@gmail.com>
2025-06-14 15:06:47 +0200

    Update main.js

--- yes-no/js/main.js
+++ yes-no/js/main.js
@@ -1,34 +1,33 @@
 const btn = document.getElementById("click-to-random");
 const answer = document.querySelector(".answer");
 const rotator = document.querySelector(".output-area");
-console.log(btn);
-console.log(answer);
+const wrapper = document.querySelector(".wrapper");
+
 btn.addEventListener("click", () => {
   const number = Math.random();
-  if (number > 0.5) {
-    console.log(number);
-    rotator.classList.add("output-area-rotate");
+  const result = number > 0.5 ? "YES" : "NO";
+
+  console.log("Number:", number);
+  rotator.classList.add("output-area-rotate");
+
+  if (result === "NO") {
+    // Через 2 секунды добавить класс
     setTimeout(() => {
-      answer.textContent = "YES";
-      rotator.classList.remove("output-area-rotate");
-      console.log(number);
-    }, 2500);
+      wrapper.classList.add("wrapperBad");
+    }, 2000);
+
+    // Удалить класс через 4 секунды (т.е. через 2 после добавления)
     setTimeout(() => {
-      console.log(number);
-      answer.textContent = "";
-      rotator.classList.remove("output-area-rotate");
-    }, 4000);
-  } else {
-    rotator.classList.add("output-area-rotate");
-    setTimeout(() => {
-      console.log(number);
-      answer.textContent = "NO";
-      rotator.classList.remove("output-area-rotate");
-    }, 2500);
-    setTimeout(() => {
-      console.log(number);
-      answer.textContent = "";
-      rotator.classList.remove("output-area-rotate");
+      wrapper.classList.remove("wrapperBad");
     }, 4000);
   }
+
+  setTimeout(() => {
+    answer.textContent = result;
+    rotator.classList.remove("output-area-rotate");
+  }, 2500);
+
+  setTimeout(() => {
+    answer.textContent = "";
+  }, 4000);
 });
